5cdc58317003649d62c01fe5d962742bcd1f5a01
protectivedad <protectivedad@gmail.com>
2019-11-09 07:29:49 -0400

    THERMAL: Moved from firmware.

---
+++ external/package/thermal/thermal.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# thermal
+#
+################################################################################
+THERMAL_VERSION = 1.0
+THERMAL_SITE = $(BR2_EXTERNAL)/sources
+THERMAL_SITE_METHOD = file
+THERMAL_SOURCE = thermal.tar.xz
+
+define THERMAL_EXTRACT_CMDS
+	tar -C $(@D) -xf ${DL_DIR}/${THERMAL_SOURCE}
+endef
+
+define THERMAL_BUILD_CMDS
+	echo "No building required."
+endef
+
+define THERMAL_INSTALL_TARGET_CMDS
+	cd $(@D); for mode_755 in `find -L . -type f -not -path '*/.*'`; do \
+	   if [ ! -L $(@D)/$$mode_755 ]; then \
+	      cp -af $(@D)/$$mode_755 $(TARGET_DIR)/$$mode_755; \
+	   else \
+	      $(INSTALL) -D -m 0755 $(@D)/$$mode_755 $(TARGET_DIR)/$$mode_755; \
+	   fi \
+	done
+endef
+
+$(eval $(generic-package))
