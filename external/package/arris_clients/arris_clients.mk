42e8a3ac4609969355679153d1d71c9e6c46d997
protectivedad <protectivedad@gmail.com>
2019-11-11 11:10:15 -0400

    ARRIS_CLIENTS: Added binaries from firmware.

---
+++ external/package/arris_clients/arris_clients.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# arris_clients
+#
+################################################################################
+ARRIS_CLIENTS_VERSION = 1.0
+ARRIS_CLIENTS_SITE = $(BR2_EXTERNAL)/sources
+ARRIS_CLIENTS_SITE_METHOD = file
+ARRIS_CLIENTS_SOURCE = arris_clients.tar.xz
+
+define ARRIS_CLIENTS_EXTRACT_CMDS
+	tar -C $(@D) -xf ${DL_DIR}/${ARRIS_CLIENTS_SOURCE}
+endef
+
+define ARRIS_CLIENTS_BUILD_CMDS
+	echo "No building required."
+endef
+
+define ARRIS_CLIENTS_INSTALL_TARGET_CMDS
+	cd $(@D); for mode_755 in `find -L . -type f -not -path '*/.*'`; do \
+	   if [ ! -L $(@D)/$$mode_755 ]; then \
+	      cp -af $(@D)/$$mode_755 $(TARGET_DIR)/$$mode_755; \
+	   else \
+	      $(INSTALL) -D -m 0755 $(@D)/$$mode_755 $(TARGET_DIR)/$$mode_755; \
+	   fi \
+	done
+endef
+
+$(eval $(generic-package))
