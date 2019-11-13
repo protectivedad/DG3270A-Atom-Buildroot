1fecea6b0aa9a5ed55612dc3fe03f7854b1aae93
protectivedad <protectivedad@gmail.com>
2019-11-13 10:02:38 -0400

    PAL_OSAL: Added from firmware.

---
+++ external/package/pal_osal/pal_osal.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# pal_osal
+#
+################################################################################
+PAL_OSAL_VERSION = 1.0
+PAL_OSAL_SITE = $(BR2_EXTERNAL)/sources
+PAL_OSAL_SITE_METHOD = file
+PAL_OSAL_SOURCE = pal_osal.tar.xz
+
+define PAL_OSAL_EXTRACT_CMDS
+	tar -C $(@D) -xf ${DL_DIR}/${PAL_OSAL_SOURCE}
+endef
+
+define PAL_OSAL_BUILD_CMDS
+	echo "No building required."
+endef
+
+define PAL_OSAL_INSTALL_TARGET_CMDS
+	cd $(@D); for file in `find -L . -type f -not -path '*/.*'`; do \
+	   [ ! -d $$(dirname $$file) ] && mkdir -p $$(dirname $$file) || true; \
+	   cp -af $(@D)/$$file $(TARGET_DIR)/$$file; \
+	done
+endef
+
+$(eval $(generic-package))
