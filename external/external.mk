f70a0bcfa5bf4ac4818f81ceb4584a47370aa44e
protectivedad <protectivedad@gmail.com>
2019-11-20 11:27:36 -0400

    Added openssl legacy link option.

--- external/external.mk
+++ external/external.mk
@@ -1,5 +1,14 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_DEFAULT_PATH)/package/*/*.mk))
 
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+ifeq ($(BR2_PACKAGE_OPENSSL_LEGACY),y)
+define OPENSSL_LINK_LEGACY
+	ln -fsr $(TARGET_DIR)/usr/lib/libssl.so $(TARGET_DIR)/usr/lib/libssl.so.1.0.0
+endef
+OPENSSL_POST_INSTALL_TARGET_HOOKS += OPENSSL_LINK_LEGACY
+endif
+endif
+
 ifeq ($(BR2_PACKAGE_HOSTAPD),y)
 ifeq ($(BR2_PACKAGE_HOSTAPD_QCA988X),y)
 HOSTAPD_CONFIG_SET += \
